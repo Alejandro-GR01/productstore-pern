e684597a15a7bbd50e2d8130f7d38922f3f1c474
Alejandro <alejandroguzmanr011231@gmail.com>
2026-04-09 09:15:55 -0400

    Fixing api response in user to prevent security risk

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,7 +3,7 @@ import { getUserById } from "../db/queries.ts";
 
 export async function getUser(req: Request, res: Response) {
   if (req.user.id) {
-    const user = await getUserById(req.user.id);
+    const {passwordHash, ...user} = await getUserById(req.user.id);
     return user;
   }
 }
