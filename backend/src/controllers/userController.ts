36feeaf9b0283584faaf508addcdfcfec93eb11d
Alejandro <alejandroguzmanr011231@gmail.com>
2026-04-10 15:06:02 -0400

    Fix api response my user data

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from "express";
 export async function getUser(req: Request, res: Response) {
   if (req.user) {
     const {passwordHash, ...user} = req.user
-    return res.json({user}) ;
+    return res.json({...user}) ;
   }
   return res.status(401).json({error: 'Unauthorized'})
 }
