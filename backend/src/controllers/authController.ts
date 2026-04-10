5a4cd96e3276f9647f4bae81ee28908d2e1fc34d
Alejandro <alejandroguzmanr011231@gmail.com>
2026-04-10 13:21:50 -0400

    Fixinlogin user function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,7 +3,6 @@ import { createUser, getUserByEmail } from "../db/queries.ts";
 import { checkPassword, hashPassword } from "../utils/auth.ts";
 import { type NewUser } from "../db/schema.ts";
 
-
 import { generateJWT } from "../utils/jwt.ts";
 import generateUUID from "../utils/uuid.ts";
 
@@ -25,26 +24,30 @@ export async function registerUser(req: Request, res: Response) {
     const { passwordHash: password, ...user } = await createUser(data);
     const token = generateJWT({ id: user.id });
     return res.status(200).json({ user, token });
-  } catch (err) {
-    const error = JSON.stringify(err) || new Error("Internal Server Error");
-
-    res.status(500).json({ error: error });
+  } catch (error) {
+    console.log("Error registering user", error);
+    res.status(500).json({ error: "Error registering user" });
   }
 }
 
 export async function loginUser(req: Request, res: Response) {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const userEmail = await getUserByEmail(email);
-  if (!userEmail) {
-    return res.status(404).json({ error: "User not found" });
+    const userEmail = await getUserByEmail(email);
+    if (!userEmail) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isPasword = await checkPassword(password, userEmail.passwordHash);
+    if (!isPasword) {
+      return res
+        .status(401)
+        .json({ error: "The provided password does not match this user" });
+    }
+    const token = generateJWT({ id: userEmail.id });
+    res.send(token);
+  } catch (error) {
+    console.log("Error login user", error);
+    res.status(500).json({ error: "Error ligin user" });
   }
-  const isPasword = await checkPassword(password, userEmail.passwordHash);
-  if (!isPasword) {
-    return res
-      .status(401)
-      .json({ error: "The provided password does not match this user" });
-  }
-  const token = generateJWT({ id: userEmail.id });
-  res.send(token);
 }
